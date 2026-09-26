9e9a44be81922042623d3a9fa7a8e38f3ba3484b
Kevin Pirnie <iam@kevinpirnie.com>
2026-09-26 08:11:47 -0400

    bump theme version;

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -32,7 +32,7 @@ class ComposerAutoloaderInit49a4d4575e55becb62c502463758a48c
         call_user_func(\Composer\Autoload\ComposerStaticInit49a4d4575e55becb62c502463758a48c::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
-        $loader->setApcuPrefix('362d21ae3a2bf24b55db');
+        $loader->setApcuPrefix('9bbf52cf2cf451abe66a');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit49a4d4575e55becb62c502463758a48c::$files;
