1dc2d4ba9ef6e0797b312d85789db2251a442bbc
Kevin Pirnie <iam@kevinpirnie.com>
2026-09-26 11:46:54 -0400

    respace pricing blocks

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -32,7 +32,7 @@ class ComposerAutoloaderInit49a4d4575e55becb62c502463758a48c
         call_user_func(\Composer\Autoload\ComposerStaticInit49a4d4575e55becb62c502463758a48c::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
-        $loader->setApcuPrefix('4d9d6c1946dada2650b1');
+        $loader->setApcuPrefix('011b749dc02bbe9e62b6');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit49a4d4575e55becb62c502463758a48c::$files;
